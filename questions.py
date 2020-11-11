8de92344bbb9abdf63b9af1b8cb3605d92853e50
toshiakiasakura <aflu.blossompaws@gmail.com>
2020-11-11 13:23:17 +0900

    notebook basic explanation was prepared.

---
+++ questions.py
@@ -0,0 +1,17 @@
+
+class FrameWork():
+    def __init__(self):
+        print(self.title)
+
+
+
+class q1(FrameWork):
+    self.title= "# Question1."
+    def __init__(self): 
+        super().__init__()
+
+class questions():
+    def __init__(self): 
+        self.q1 = q1
+
+
